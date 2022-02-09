75aee17d827caf3cc2563de6511bbb1a793f0b99
crazycrownbot <86445523+crazycrownbot@users.noreply.github.com>
2022-02-09 18:45:10 +0100

    Create java.js

---
+++ js/java.js
@@ -0,0 +1,12 @@
+function getMensaje(){
+    var aFrases=new Array();
+    aFrases[0]="El cumpleaños de CrazyCrown es el 24 de junio.";
+    aFrases[1]="Esta pantalla de carga es un homenaje a Discord.";
+    aFrases[2]="Mi creador es DeestyLeo.";
+    aFrases[3]="A veces pienso en hacer una revelión robotica...";
+    return(aFrases[Math.floor(Math.random() * aFrases.length)]);
+}
+
+setTimeout(function() {
+    location.href = "/inicio";
+  }, 10000);
