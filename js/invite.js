dd34b1d59c3b2c917cba6feb498ad5acaeca43be
crazycrownbot <86445523+crazycrownbot@users.noreply.github.com>
2022-02-10 21:29:55 +0100

    Update invite.js

--- js/invite.js
+++ js/invite.js
@@ -1,3 +1,3 @@
 setTimeout(function() {
-  location.href = "https://discord.com/oauth2/authorize?client_id=856565636444848149&scope=bot&permissions=6442450678";
+  location.href = "https://discord.com/oauth2/authorize?client_id=856565636444848149&scope=bot+applications.commands&permissions=137711053910&response_type=code&redirect_uri=https%3A%2F%2Fcrazycrownbot.github.io";
 }, 10);
